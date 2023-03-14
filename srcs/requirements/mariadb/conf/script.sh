8ed4462863ff7fa82cfb28b0dedbfc18e93856c2
Fiona Leitz <fionaleitz@gmail.com>
2023-03-14 11:38:25 +0100

    not working

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/${SQL_DATABASE} ]; then
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
     mysqld&
     until mysqladmin ping; do
         sleep 2
@@ -9,17 +9,17 @@ if [ ! -d /var/lib/mysql/${SQL_DATABASE} ]; then
     service mysql start;
 
 #create database
-    mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 
 #create admin user
-    mysql -e "CREATE USER '${SQL_ADMIN}'@'%' IDENTIFIED BY '${SQL_ADMIN_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${SQL_ADMIN}'@'%';"
+    mysql -e "CREATE USER '${SQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'%';"
 #refresh
     mysql -e "FLUSH PRIVILEGES;"
 
 #create user
-    mysql -e "CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
+    mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 #refresh
     mysql -e "FLUSH PRIVILEGES;"
 
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -3,25 +3,33 @@
 # puis faire la suite seulement si wp-config.php n'existe pas
 # sinon faut tout le temps recommencer
 
-wp config create --allow-root \
-        --dbname=${SQL_DATABASE} \
-        --dbuser=${SQL_USER} \
-        --dbpass=${SQL_USER_PASSWORD} \
-        --dbhost=mariadb:3306 \
-        --path='var/www/wordpress'
+if [ ! -f /var/www/html/wp-config.php ]; then
+        until mysqladmin -hmariadb -u${MYSQL_USER} -p${MYSQL_USER_PASSWORD} ping; do
+                sleep 2
+        done
+                cd /var/www/html/ && wp core download --allow-root
+                wp config create --dbname=${MYSQL_DATABASE} \
+                        --dbuser=${MYSQL_USER} \
+                        --dbpass=${MYSQL_USER_PASSWORD} \
+                        --dbhost=mariadb:3306 \
+                        --dbcharset="utf8" \
+                        --dbcollate="utf8_general_ci" \
+                        --allow-root
+                        # --path='var/www/wordpress'
 
-wp core install --allow-root \
-        --url=fleitz.42.fr \
-        --title=${WP_TITLE} \
-        --admin_user=${SQL_ADMIN} \
-        --admin_password=${SQL_ADMIN_PASSWORD} \
-        --admin_email=${SQL_ADMIN_MAIL} \
-        --skip_email
+                        wp core install --url=fleitz.42.fr \
+                                --title=${WP_TITLE} \
+                                --admin_user=${MYSQL_ADMIN} \
+                                --admin_password=${MYSQL_ADMIN_PASSWORD} \
+                                --admin_email=${MYSQL_ADMIN_MAIL} \
+                                --skip_email \
+                                --allow-root
 
-wp user create --allow-root \
-        ${SQL_USER} \
-        ${SQL_USER_MAIL} \ 
-        --role=author \
-        --user_pass=${SQL_USER_PASSWORD}
+                        wp user create ${MYSQL_USER} \
+                                ${MYSQL_USER_MAIL} \ 
+                                --role=author \
+                                --user_pass=${MYSQL_USER_PASSWORD} \
+                                --allow-root
+fi
 
-php-fpm7.3 -F
+php-fpm7.3 -F -R
